3b6db46e5d33cd77a4ff30fca7c10aa446ae0cfb
AnnaTari <anata16@outlook.de>
2023-09-28 18:32:03 +0200

    init structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { AdminEditComponent } from './admin-edit/admin-edit.component';
+import { EventPageComponent } from './event-page/event-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    LoginComponent,
+    AdminEditComponent,
+    EventPageComponent
   ],
   imports: [
     BrowserModule
