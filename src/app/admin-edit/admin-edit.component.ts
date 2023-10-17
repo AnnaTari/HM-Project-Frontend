4c55a1711a919b00b5b65b8be9c99f641276bb2e
AnnaTari <anata16@outlook.de>
2023-10-17 19:15:45 +0200

    first tab of admin-edit with example list

--- src/app/admin-edit/admin-edit.component.ts
+++ src/app/admin-edit/admin-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {EventModel} from "../shared/models/event.model";
 
 @Component({
   selector: 'app-admin-edit',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AdminEditComponent {
 
+   actualEvents: EventModel[] = [
+    {id: 1, title:'Superman'},
+    {id: 2, title:'Batman'},
+    {id: 5, title:'BatGirl'},
+    {id: 3, title:'Robin'},
+    {id: 4, title:'Flash'}
+  ];
+
+  futureEvents: EventModel[] = [
+    {id: 1, title:'HSV'},
+    {id: 2, title:'Football'},
+    {id: 5, title:'BatGivxf'},
+    {id: 3, title:'Robinfdv'},
+    {id: 4, title:'Flashfxv'}
+  ];
 }
