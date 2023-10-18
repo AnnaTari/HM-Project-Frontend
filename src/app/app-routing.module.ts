664f094b8701e7623f3747e05cac1adf621a7deb
Yasch-Gahi <yasch.gahi@myhsba.de>
2023-10-18 17:51:49 +0200

    popup 1. try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminEditComponent} from "./admin-edit/admin-edit.component";
+import {EventPageComponent} from "./event-page/event-page.component";
 
 //defines your routes
 const routes: Routes = [
   {path: 'home', title: "Homepage", component: HomepageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'admin-edit', component: AdminEditComponent},
+  {path: 'event-page', component: EventPageComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}, //route that defaults to the home-component
-  {path: '**', component: HomepageComponent} //wildcard route - in case the user attempt to navigate where nothing exists
+  {path: '**', component: HomepageComponent}//wildcard route - in case the user attempt to navigate where nothing exists
 ];
 
 @NgModule({
