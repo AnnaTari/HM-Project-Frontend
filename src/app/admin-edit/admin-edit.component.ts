ea00998d2b96ebd9ffec925414e94c68a72197c2
AnnaTari <anata16@outlook.de>
2023-10-18 16:18:05 +0200

    first admin-edit structure

--- src/app/admin-edit/admin-edit.component.ts
+++ src/app/admin-edit/admin-edit.component.ts
@@ -11,16 +11,13 @@ export class AdminEditComponent {
    actualEvents: EventModel[] = [
     {id: 1, title:'Superman'},
     {id: 2, title:'Batman'},
-    {id: 5, title:'BatGirl'},
-    {id: 3, title:'Robin'},
-    {id: 4, title:'Flash'}
+    {id: 5, title:'BatGirl',}
   ];
 
   futureEvents: EventModel[] = [
     {id: 1, title:'HSV'},
     {id: 2, title:'Football'},
-    {id: 5, title:'BatGivxf'},
-    {id: 3, title:'Robinfdv'},
-    {id: 4, title:'Flashfxv'}
   ];
+
+
 }
