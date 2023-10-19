a7b7e4208259dc4df2a42df2c6b2fe44cb9068a2
Koechel <lara.koechel@myhsba.de>
2023-10-19 15:26:49 +0200

    Header redirects finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor (private router: Router) { }
+
+//Methods to navigate to...
+  navigateToTermsAndConditions() {
+    this.router.navigate(['./terms-and-conditions']);
+  }
+
+  navigateToHomepage() {
+    this.router.navigate(['./homepage']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['./login']);
+  }
 }
+
+
