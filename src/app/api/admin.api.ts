ea4ec8d1ab525378558d1ef2f18af0b79b208d62
AnnaTari <anata16@outlook.de>
2023-10-21 19:35:24 +0200

    admin login basics work

--- src/app/api/admin.api.ts
+++ src/app/api/admin.api.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {UserModel} from "../shared/models/user.model";
 import {Observable} from "rxjs";
-import {CurrentStateService} from "../shared/services/current-state.service";
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +12,8 @@ export class AdminApi {
   constructor(private httpClient: HttpClient) {
   }
 
-  public getAdmin(admin: { adminName: string, adminPassword: string }) {
+  public login(admin: { adminName: string, adminPassword: string }): Observable<UserModel> {
     console.log("Vor Abschicken" + admin.adminName, admin.adminPassword);
     return this.httpClient.post <UserModel>(this.endpoint, admin);
   }
-
-
 }
