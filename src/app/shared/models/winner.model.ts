7e3c0098ce2367151d7e9630af066f36fa03142a
Koechel <lara.koechel@myhsba.de>
2023-10-30 12:29:45 +0100

    Event api working! Adapting names with EventModel left

--- src/app/shared/models/winner.model.ts
+++ src/app/shared/models/winner.model.ts
@@ -1,9 +1,11 @@
 export interface WinnerModel {
   userId: number,
   eventHsvId: number,
+  blacklistId: number,
   name: string,
   email: string,
   escortName: string,
   substituteWinner: boolean,
+  blacklistCounter: number,
   //....
 }
