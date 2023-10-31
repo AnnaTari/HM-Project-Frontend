23e0af8ee5cc57c6d20d6c84b8bc186920965ca8
AnnaTari <anata16@outlook.de>
2023-10-31 16:19:24 +0100

    successfully added events

--- src/app/admin-edit/edit-event/edit-event.component.ts
+++ src/app/admin-edit/edit-event/edit-event.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {EventModel} from "../../shared/models/event.model";
+import {CurrentStateService} from "../../shared/services/current-state.service";
+import {EventApi} from "../../api/event.api";
 
 @Component({
   selector: 'app-edit-event',
@@ -9,12 +12,12 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class EditEventComponent {
   eventForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private currentStateService: CurrentStateService, private eventApi: EventApi) {
     this.eventForm = this.fb.group({
       matchName: ['', Validators.required],
       matchDetails: ['', Validators.required],
       matchDate: [Date, Validators.required],
-      matchTime: ['', Validators.required],
+      matchTime: [''],
       location: ['Volksparkstadion', Validators.required],
       gamePicture: [],
       deadline: [Date, Validators.required],
@@ -22,6 +25,30 @@ export class EditEventComponent {
       ticketAmount: [0, Validators.required],
       registrationDate: [Date, Validators.required],
     })
+  }
+
+  onSubmit() {
+    console.log("Hallo");
+    let adminId = 0;
+    this.currentStateService.getAdminObs().subscribe((admin) => {
+      adminId = admin.adminId ? admin.adminId : 0;
+    })
+    let eventDate = new Date(this.eventForm.value.matchDate);
+    //eventDate.setTime(this.eventForm.value.matchTime);
+    let event: EventModel = {
+      eventHsvId: 0,
+      adminId: adminId,
+      matchName: this.eventForm.value.matchName,
+      matchDetails: this.eventForm.value.matchDetails,
+      eventDate: eventDate,
+      location: this.eventForm.value.location,
+      deadline: this.eventForm.value.deadline,
+      ticketType: this.eventForm.value.ticketType,
+      ticketAmount: this.eventForm.value.ticketAmount,
+      registrationDate: this.eventForm.value.registrationDate
+    }
+    console.log(event);
+    this.eventApi.addEvent(event).subscribe((events) => this.currentStateService.separateActualAndFutureEvents(events));
   }
 
   //When you edit events you need to patch the value --> name of form should be identical to EventModel
