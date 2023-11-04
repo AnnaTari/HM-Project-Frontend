a1a3ee364c37b452de599b7d058fc67a2bec9777
Koechel <lara.koechel@myhsba.de>
2023-11-04 13:29:56 +0100

    preparing radio buttons, not working

--- src/app/raffle-page/raffle-page.component.ts
+++ src/app/raffle-page/raffle-page.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Observable} from "rxjs";
+import {EventModel} from "../shared/models/event.model";
+import {WinnerApi} from "../api/winner.api";
+import {EventApi} from "../api/event.api";
+import {CurrentStateService} from "../shared/services/current-state.service";
 
 @Component({
   selector: 'app-raffle-page',
@@ -6,5 +11,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./raffle-page.component.css']
 })
 export class RafflePageComponent {
+  expiredEvents$: Observable<EventModel[]>;
+  today: Date = new Date;
+
+  constructor(private eventApi: EventApi, private winnerApi: WinnerApi, private currentStateService: CurrentStateService) {
+    this.eventApi.check().subscribe((data)=> this.currentStateService.setEventObs(data));
+    this.expiredEvents$ = this.currentStateService.getExpiredEvents();
+    //this.winnerApi.check().
+  }
+
+  @Input()
+  event: any= {eventHsvId: 0, adminId: 0, matchName: "", matchDetails: "", event_date: new Date(), location:"", deadline: new Date(), ticketType: 0, ticketAmount: 0, registrationDate: new Date()};
 
 }
