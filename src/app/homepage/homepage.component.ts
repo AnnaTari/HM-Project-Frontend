6cbd3d30fa9839cd7be99f7191f2ea82498a06ea
Koechel <lara.koechel@myhsba.de>
2023-11-28 20:19:51 +0100

    layout adaption and comments added

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,12 +12,16 @@ import {DomSanitizer} from "@angular/platform-browser";
 })
 export class HomepageComponent implements OnInit{
 
+  //declare Observables
   events$: Observable<EventWithPictureModel[]>;
   actualEvents$: Observable<EventWithPictureModel[]>;
   futureEvents$: Observable<EventWithPictureModel[]>;
+
+  //property of type date
   today: Date = new Date;
   showImage: any;
 
+  //fetch event data from eventApi, setting event observation in current state service, retrieve actual and future events
   constructor(private eventApi: EventApi, private currentStateService: CurrentStateService, private sanitizer:DomSanitizer) {
     this.eventApi.check().subscribe((data)=> this.currentStateService.setEventObs(data));
     this.actualEvents$ = this.currentStateService.getActualEvents();
@@ -25,16 +29,19 @@ export class HomepageComponent implements OnInit{
     this.events$ = this.currentStateService.getEventObs();
   }
 
+  //define Input property event and provide default values
   @Input()
   event: EventWithPictureModel= {eventHsvId: 0, adminId: 0, matchName: "", matchDetails: "", eventDate: new Date(), location:"", deadline: new Date(), ticketType: 0, ticketAmount: 0, registrationDate: new Date(), picture: new Uint8Array([])};
 
+  //method to generate URL for displaying a base64 image
   transform(base64Image: Uint8Array) {
     return this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + base64Image);
   }
+
   ngOnInit() {
-    this.showImage = this.transform(this.event.picture);
-    this.eventApi.check().subscribe((data => {
-      this.currentStateService.separateActualAndFutureEvents(data);
+    this.showImage = this.transform(this.event.picture); //call transform method to assign to showImage property
+    this.eventApi.check().subscribe((data => { //call check method of eventApi to retrieve event data
+      this.currentStateService.separateActualAndFutureEvents(data); //separate actual and future events
     }))
 
   }
