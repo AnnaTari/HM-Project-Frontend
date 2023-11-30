714c6b10fbec16cdb4d3c0537d6bb5ef3925caeb
AnnaTari <anata16@outlook.de>
2023-11-30 10:07:55 +0100

    implemented another authGuard

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -3,6 +3,27 @@ import {Router} from '@angular/router';
 
 import {AuthService} from './auth.service';
 
+export const authLoginGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  let isLoggedIn: boolean = true;
+
+  authService.getLoggedIn().subscribe((loggedIn) => {
+    console.log(loggedIn);
+    isLoggedIn = loggedIn;
+  })
+
+  if (isLoggedIn) {
+    // Redirect authenticated users away from the login page
+    return router.navigate(['/admin-edit']);
+  }
+
+  // Allow access to the login page for non-authenticated users
+  return true;
+};
+
+
 export const authGuard = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
@@ -15,9 +36,10 @@ export const authGuard = () => {
   })
 
   if (isLoggedIn) {
+    // Allow access for authenticated users
     return true;
   }
 
-  // Redirect to the login page
+  // Redirect non-authenticated users to the login page
   return router.parseUrl('/login');
 };
